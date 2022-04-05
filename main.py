d25e9899cd6ff3dac1737a3e1aa1e125ade402ba
Nikolozi Mouravidze <nikolozi.mouravidze@quantori.com>
2022-04-05 13:25:01 +0400

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ BULLET_VEL = 10
 BIG_BULLET_VEL = 20
 MAX_BULLETS = 3
 MAX_B_BULLETS = 1
-SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 40, 55
 
 YELLOW_HIT = pygame.USEREVENT + 1
 RED_HIT = pygame.USEREVENT + 2
@@ -40,12 +40,12 @@ YELLOW_BIG_HIT = pygame.USEREVENT + 4
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(
     os.path.join('Assets', 'spaceship_yellow.png'))
 YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
-    YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+    YELLOW_SPACESHIP_IMAGE, (SPACESHIP_HEIGHT, SPACESHIP_WIDTH)), 90)
 
 RED_SPACESHIP_IMAGE = pygame.image.load(
     os.path.join('Assets', 'spaceship_red.png'))
 RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
-    RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
+    RED_SPACESHIP_IMAGE, (SPACESHIP_HEIGHT, SPACESHIP_WIDTH)), 270)
 
 BACKGROUND_IMAGE = pygame.image.load(os.path.join('Assets', 'space.png'))
 BACKGROUND = pygame.transform.scale(BACKGROUND_IMAGE, (WIDTH, HEIGHT))
